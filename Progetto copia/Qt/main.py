dd6ebb0e3638b7d1aa2e5b3d669860150d3d7ae4
manufara <emafaraglia@hotmail.com>
2024-10-03 18:21:06 +0200

    a

--- Progetto copia/Qt/main.py
+++ Progetto copia/Qt/main.py
@@ -1,8 +1,8 @@
 import os
-from menu_class import Piatto, MenuClass#, Comanda
+from menu_class import leggi_menu_da_file
 from PyQt5.QtWidgets import *
-from PyQt5 import uic
-
+from PyQt5 import uic, QtWidgets
+from PyQt5.QtCore import Qt
 
 # Classe per la prima finestra (home_page)
 class HomePage(QMainWindow):
@@ -113,18 +113,17 @@ class PrenotaCliente(QMainWindow):
         # Carica la finestra prenota
         ui_file = os.path.join(os.path.dirname(__file__), "prenota.ui")
         uic.loadUi(ui_file, self)
-
         # Memorizza la finestra precedente
         self.previous_window = previous_window
 
         # Crea un gruppo di pulsanti, aggiunge le checkbox al gruppo e rende il gruppo mutualmente esclusivo
         self.button_group = QButtonGroup(self)
-        self.button_group.addButton(self.checkBox)
-        self.button_group.addButton(self.checkBox_2)
+        self.button_group.addButton(self.pranzo_check)
+        self.button_group.addButton(self.cena_check)
         self.button_group.setExclusive(True)
 
         # Collega il pulsante di conferma prenotazione
-        #self.findChild(QPushButton, 'pushButton').clicked.connect(self.----------)
+        #self.findChild(QPushButton, 'pushButton').clicked.connect(self.crea_prenotazione)
         # Collega il pulsante per tornare indietro
         self.findChild(QPushButton, 'indietro').clicked.connect(self.open_indietro)
 
@@ -177,6 +176,29 @@ class Menu(QMainWindow):
         # Carica la finestra menu
         ui_file = os.path.join(os.path.dirname(__file__), "menu.ui")
         uic.loadUi(ui_file, self)
+        # Imposta il QListWidget per non essere modificabile
+        self.menu_list.setEditTriggers(QListWidget.NoEditTriggers)
+
+        # Creazione di un menu leggendo dal file di testo
+        menu = leggi_menu_da_file('Progetto copia/Qt/testo_menu.txt')
+
+        # Set per tracciare le categorie già visualizzate
+        categorie_visualizzate = set()
+
+        # Popola la lista con categorie e piatti
+        for piatto in menu.piatti:
+            # Se la categoria del piatto non è stata ancora visualizzata, aggiungila
+            if piatto.categoria not in categorie_visualizzate:
+                # Aggiungi una riga vuota tra le categorie, tranne per la prima
+                if categorie_visualizzate:
+                    self.menu_list.addItem('')  # Riga vuota per separare le categorie
+                
+                # Aggiungi la categoria
+                self.menu_list.addItem(piatto.categoria)
+                categorie_visualizzate.add(piatto.categoria)  # Segna la categoria come visualizzata
+
+            # Aggiungi il piatto sotto la categoria corretta
+            self.menu_list.addItem(piatto.mostraPiatto())
 
         # Collega il pulsante per tornare indietro
         self.findChild(QPushButton, 'pushButton').clicked.connect(self.open_home_cliente)
@@ -213,6 +235,8 @@ class Info(QMainWindow):
     def load_text_from_file(self, filename):
         with open(filename, 'r', encoding='utf-8') as file:
             content = file.read()
+            # Imposta l'allineamento giustificato
+            #self.textEdit.setAlignment(Qt.AlignJustify)
             self.textEdit.setPlainText(content)
 
 # Classe per la finestra (login) --------------------------------------------------------------------------------------
@@ -265,7 +289,7 @@ class HomeCameriere(QMainWindow):
         uic.loadUi(ui_file, self)
 
         # Collega il pulsante prenotazioni
-        #self.findChild(QPushButton, 'visualizza_lista').clicked.connect(self.modifica_admin)
+        #self.findChild(QPushButton, 'prenotazioni').clicked.connect(self.open_prenotazioni)
         # Collega il pulsante nuovo_ordine
         self.findChild(QPushButton, 'nuovo_ordine').clicked.connect(self.open_ricerca)
         # Collega il pulsante aggiorna_ordine
@@ -290,7 +314,7 @@ class RicercaTavolo(QMainWindow):
     def __init__(self):
         super(RicercaTavolo, self).__init__()
 
-        # Carica la finestra home_amministratore
+        # Carica la finestra cerca_tavolo
         ui_file = os.path.join(os.path.dirname(__file__), "cerca_tavolo.ui")
         uic.loadUi(ui_file, self)
 
@@ -315,6 +339,8 @@ class HomeAmministratore(QMainWindow):
 
         # Collega il pulsante per aprire prenotazioni_cliente
         self.findChild(QPushButton, 'prenotazioni').clicked.connect(self.open_prenotazioni)
+        # Collega il pulsante stampa_conto
+        self.findChild(QPushButton, 'stampa').clicked.connect(self.open_stampa)
         # Collega il pulsante modifica
         self.findChild(QPushButton, 'modifica').clicked.connect(self.modifica_admin)
         # Collega il pulsante per tornare indietro
@@ -327,6 +353,11 @@ class HomeAmministratore(QMainWindow):
         self.pren_cli_window.show()
         self.close()
 
+    def open_stampa(self):
+        self.cliente_window = StampaConto()
+        self.cliente_window.show()
+        self.close()
+
     def modifica_admin(self):
         self.cliente_window = ModificaAdmin()
         self.cliente_window.show()
@@ -334,6 +365,25 @@ class HomeAmministratore(QMainWindow):
 
     def open_indietro(self):
         self.cliente_window = Login()
+        self.cliente_window.show()
+        self.close()
+
+# Classe per la finestra (stampa_conto)------------------
+class StampaConto(QMainWindow):
+    def __init__(self):
+        super(StampaConto, self).__init__()
+
+        # Carica la finestra stampa_conto
+        ui_file = os.path.join(os.path.dirname(__file__), "stampa_conto.ui")
+        uic.loadUi(ui_file, self)
+
+        # Collega il pulsante per tornare indietro
+        self.findChild(QPushButton, 'indietro').clicked.connect(self.open_indietro)
+
+        self.show()
+
+    def open_indietro(self):
+        self.cliente_window = HomeAmministratore()
         self.cliente_window.show()
         self.close()
 
@@ -379,13 +429,131 @@ class ModificaMenu(QMainWindow):
         ui_file = os.path.join(os.path.dirname(__file__), "menu.ui")
         uic.loadUi(ui_file, self)
 
+        # Creazione di un menu leggendo dal file di testo
+        menu = leggi_menu_da_file('Progetto copia/Qt/testo_menu.txt')
+
+        # Set per tracciare le categorie già visualizzate
+        categorie_visualizzate = set()
+
+        # Popola la lista con categorie e piatti
+        for piatto in menu.piatti:
+            # Se la categoria del piatto non è stata ancora visualizzata, aggiungila
+            if piatto.categoria not in categorie_visualizzate:
+                # Aggiungi una riga vuota tra le categorie, tranne per la prima
+                if categorie_visualizzate:
+                    self.menu_list.addItem('')  # Riga vuota per separare le categorie
+                
+                # Aggiungi la categoria
+                self.menu_list.addItem(piatto.categoria)
+                categorie_visualizzate.add(piatto.categoria)  # Segna la categoria come visualizzata
+
+            # Aggiungi il piatto sotto la categoria corretta
+            self.menu_list.addItem(piatto.mostraPiatto())
+
         # Rende modificabile il testo e visibile il pulsante salva
         self.menu_list.setEnabled(True)
         self.salva.setGeometry(80, 380, 101, 31)
-        # Collega il pulsante indietro
+        # Collega il pulsante indietro e il pulsante salva
+        self.findChild(QPushButton, 'salva').clicked.connect(self.salva_modifiche_al_file)
         self.findChild(QPushButton, 'pushButton').clicked.connect(self.modifica_admin)
 
+        # Trova il QListWidget
+        self.list_widget = self.findChild(QListWidget, 'menu_list')
+        # Imposta i trigger di modifica, in questo caso doppio clic
+        self.list_widget.setEditTriggers(QtWidgets.QAbstractItemView.DoubleClicked)
+
+        # Aggiungi il menu contestuale (clic destro)
+        self.list_widget.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.list_widget.customContextMenuRequested.connect(self.show_context_menu)
+
         self.show()
+
+    # Funzione per il menu contestuale
+    def show_context_menu(self, position):
+        menu = QtWidgets.QMenu()
+
+        # Imposta il foglio di stile
+        menu.setStyleSheet("""
+            QMenu {
+                background-color: #F0F0F0;
+                border: 2px solid #5A5A5A;
+                border-radius: 10px;
+                padding: 5px;
+                margin: 0;  /* Aggiunto per evitare margini */
+            }
+            QMenu::item {
+                background-color: transparent;
+                color: #5A5A5A;
+                padding: 5px 20px;
+                border-radius: 5px;
+            }
+            QMenu::item:selected {
+                background-color: #D3D3D3;
+            }
+            QMenu::item:pressed {
+                background-color: #A9A9A9;
+            }
+        """)
+
+        add_action = menu.addAction("Aggiungi Elemento")
+        edit_action = menu.addAction("Modifica Elemento")
+        delete_action = menu.addAction("Elimina Elemento")
+
+        action = menu.exec_(self.list_widget.viewport().mapToGlobal(position))
+
+        if action == add_action:
+            self.add_item()
+        elif action == edit_action:
+            self.edit_item()
+        elif action == delete_action:
+            self.delete_item()
+
+    # Aggiungi un nuovo elemento alla lista
+    def add_item(self):
+        text, ok = QInputDialog.getText(self, 'Aggiungi Elemento', 'Inserisci il nome del nuovo elemento:')
+        if ok and text:
+            row = self.list_widget.currentRow()  # Ottieni la riga attualmente selezionata
+            self.list_widget.insertItem(row, text)  # Inserisci l'elemento nella posizione selezionata
+
+    # Modifica l'elemento selezionato
+    def edit_item(self):
+        selected_item = self.list_widget.currentItem()
+        if selected_item:
+            dialog = QInputDialog(self)
+            dialog.setWindowTitle('Modifica Elemento')
+            dialog.setLabelText('Modifica il testo:')
+            dialog.setTextValue(selected_item.text())
+            
+            # Imposta una dimensione maggiore per la finestra
+            dialog.resize(400, 100)  # Modifica la dimensione del dialogo qui (larghezza, altezza)
+            
+            if dialog.exec_() == QtWidgets.QDialog.Accepted:
+                new_text = dialog.textValue()
+                if new_text:
+                    selected_item.setText(new_text)
+
+    # Elimina l'elemento selezionato
+    def delete_item(self):
+        selected_item = self.list_widget.currentItem()
+        if selected_item:
+            self.list_widget.takeItem(self.list_widget.row(selected_item))
+
+    def salva_modifiche_al_file(self):
+        with open('Progetto copia/Qt/testo_menu.txt', 'w') as file:
+            categoria_corrente = None
+            for i in range(self.list_widget.count()):
+                item = self.list_widget.item(i).text()
+                
+                # Controlla se è una categoria
+                if not (":" in item and "€" in item):  # Se non contiene il formato piatto: descrizione - prezzo€
+                    categoria_corrente = item.strip()
+                    file.write(categoria_corrente + '\n')  # Scrivi la categoria nel file
+                else:
+                    # Se è un piatto, riconverti al formato originale
+                    piatto, resto = item.split(": ")
+                    descrizione, prezzo = resto.split(" - ")
+                    prezzo = prezzo.replace("€", "")  # Rimuovi il simbolo dell'euro
+                    file.write(f"{piatto}, {descrizione}, {prezzo}\n")
 
     def modifica_admin(self):
         self.cliente_window = ModificaAdmin()
@@ -418,7 +586,6 @@ class ModificaInfo(QMainWindow):
         self.cliente_window.show()
         self.close()
 
-    # Aggiungi questo metodo nella tua classe MainWindow
     def salva_modifiche_al_file(self):
         contenuto = self.textEdit.toPlainText()  # Ottiene il testo dal QTextEdit
         with open('Progetto copia/Qt/testo_info.txt', 'w', encoding='utf-8') as file:
@@ -433,10 +600,8 @@ class ModificaInfo(QMainWindow):
 # Funzione principale
 def main():
     app = QApplication([])
-
     # Avvia la prima finestra (home_page)
     window = HomePage()
-
     app.exec()
 
 if __name__ == '__main__':
--- Progetto copia/Qt/menu_class.py
+++ Progetto copia/Qt/menu_class.py
@@ -15,76 +15,7 @@ class MenuClass():
     def __init__(self, piatti):
         self.piatti = piatti
 
-"""    # ------------
-    def ModificaMenu(self):
-        while True:
-            print("\n--- Modifica Menu ---")
-            print("1. Aggiungi piatto")
-            print("2. Elimina piatto")
-            print("3. Modifica piatto")
-            print("4. Esci")
-            scelta = input("Inserisci il numero corrispondente alla tua scelta: ")
-
-            if scelta == '1':
-                # Aggiungi un piatto
-                numero = len(self.piatti)+1
-                nome = input("inserisci nome del piatto: ")
-                descrizione = input("inserisci descrizione del piatto: ")
-                prezzo = input("inserisci il prezzo del piatto: ")
-                new_piatto = Piatto.Piatto(numero, nome, descrizione, prezzo)
-                self.piatti.append(new_piatto)
-
-            elif scelta == '2':
-                # Elimina un piatto
-                self.mostraMenu()
-                while True:
-                    piatto_da_eliminare = int(input("'esci' o Inserisci il numero del piatto da eliminare: "))
-                    if piatto_da_eliminare == "esci":
-                        return
-                    for piatto in self.piatti:
-                        if piatto.numero == piatto_da_eliminare:
-                            del self.piatti[piatto_da_eliminare-1]
-                            print("piatto eliminato correttamente dal menu")
-                            return
-
-                    print("piatto non trovato, riprova")
-
-            elif scelta == '3':
-                # Modifica un piatto
-                piatto_da_modificare = int(input("'esci' o Inserisci il numero del piatto da modificare: "))
-                if piatto_da_modificare == "esci":
-                    return
-                for piatto in self.piatti:
-                    if piatto.numero == piatto_da_modificare:
-
-                        print("Cosa vuoi modificare?")
-                        print("1. Prezzo")
-                        print("2. Descrizione")
-                        print()
-                        modifica = input("Inserisci il numero della tua scelta: ")
-
-                        if modifica == '1':
-                            nuovo_prezzo = float(input(f"Inserisci il nuovo prezzo per '{piatto_da_modificare}': "))
-                            piatto.prezzo = nuovo_prezzo
-                            print(f"Prezzo di '{piatto}' modificato con successo.")
-
-                        elif modifica == '2':
-                            nuova_descrizione = input(f"Inserisci la nuova descrizione per '{piatto_da_modificare}': ")
-                            piatto = nuova_descrizione
-                            print(f"Descrizione di '{piatto}' modificata con successo.")
-                        else:
-                            print("Scelta non valida.")
-                    else:
-                        print(f"Il piatto indicato non è presente nel menu.")
-
-            elif scelta == '4':
-                print("Uscita dal menu di modifica.")
-                break
-            else:
-                print("Scelta non valida, riprova.")
-
-
-# classe comanda ----------------------------------
+"""# classe comanda ----------------------------------
 class Comanda:
     def __init__(self, cameriere):
         self.cameriere = cameriere
@@ -107,14 +38,33 @@ class Comanda:
                     self.totale=+piatto.prezzo
                     print ("piatto aggiunto alla comanda")
                     aggiunto=True
-            if not aggiunto : print("riprova")
+            if not aggiunto : print("riprova")"""
 
+# Funzione per leggere il menu da un file con categorie e piatti e creare oggetti Piatto e MenuClass
+def leggi_menu_da_file(file_path):
+    piatti = []  # Lista che conterrà oggetti Piatto
+    categoria_corrente = None  # Per tenere traccia della categoria attuale
+    
+    with open(file_path, 'r') as file:
+        for linea in file:
+            linea = linea.strip()
+            
+            # Ignora le righe vuote
+            if not linea:
+                continue
+            
+            # Se la riga non contiene una virgola, la trattiamo come categoria
+            if ',' not in linea:
+                categoria_corrente = linea  # Aggiorna la categoria corrente
+                continue
+            
+            # Se la riga contiene una virgola, la trattiamo come piatto
+            try:
+                nome, descrizione, prezzo = linea.split(',')
+                piatto = Piatto(categoria_corrente, nome, descrizione.strip(), prezzo.strip())  # Crea un oggetto Piatto
+                piatti.append(piatto)  # Aggiungi l'oggetto Piatto alla lista
+            except ValueError:
+                print(f"Errore nel formato della riga: {linea}")
 
-# inizializzazione ---------------------------------
-piatto1=Piatto("Moscioli", "Antipasto", 5)
-piatto2=Piatto("Gnocchi", "Primo", 10)
-piatto3=Piatto("Frittura","secondo",15)
-piatto4=Piatto("tiramisu","Dolce",4)
-piatti=[piatto1,piatto2,piatto3,piatto4]
-
-menu=MenuClass(piatti) """
+    menu = MenuClass(piatti)  # Crea un oggetto MenuClass con i piatti
+    return menu
