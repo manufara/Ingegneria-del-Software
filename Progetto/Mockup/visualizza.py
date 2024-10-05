b9f6213be972472eef51baad780b37d3834557f8
manufara <emafaraglia@hotmail.com>
2024-10-05 12:14:05 +0200

    a

--- Progetto/Mockup/visualizza.py
+++ Progetto/Mockup/visualizza.py
@@ -7,10 +7,20 @@ class HomePage(QMainWindow):
         super(HomePage, self).__init__()
 
         # Carica la finestra home_page
-        ui_file = os.path.join(os.path.dirname(__file__), "home_amministratore.ui")
+        ui_file = os.path.join(os.path.dirname(__file__), "stampa_conto.ui")
         uic.loadUi(ui_file, self)
 
+        self.findChild(QPushButton, 'pushButton').clicked.connect(self.autenticazione)
+
         self.show()
+
+    def autenticazione(self):
+        if False:
+            return
+        else:
+            message = QMessageBox()
+            message.setText("Descrizione \nAntipasto di mare misto - 16 \nTagliatelle allo scoglio - 12 \nGnocchi ai frutti di mare - 13 \nSorbetto al limone - 5 \nTiramisu - 6 \n\nTotale - 52€")
+            message.exec()
 
 # Funzione principale
 def main():
